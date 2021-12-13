e7dbfa4350e81b21ff09cb993cf0c6197021ac84
elisha0904 <94889801+elisha0904@users.noreply.github.com>
2021-12-13 16:37:56 +0900

    Add files via upload

---
+++ blog_js.js
@@ -0,0 +1,12 @@
+// 햄버거 메뉴
+
+var burger = $('.menu-trigger');
+
+burger.each(function(index){
+  var $this = $(this);
+  
+  $this.on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('active-' + (index+1));
+  })
+});
