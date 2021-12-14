d5c169f2957804d50ed1430a7d3d6043711963e9
elisha0904 <94889801+elisha0904@users.noreply.github.com>
2021-12-14 16:18:44 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -129,20 +129,5 @@ function isElementUnderBottom(elem, triggerDiff) {
       fixedBackground:true
     });
 
-// 배너
-const content = "Hello! my name is Elisha.\n 만나서 반가워요!\n 저는 김현조입니다.";
-const text = document.querySelector(".text");
-let i = 0;
-
-function typing(){
-    let txt = content[i++];
-    text.innerHTML += txt=== "\n" ? "<br/>": txt;
-    if (i > content.length) {
-        text.textContent = "";
-        i = 0;
-    }
-}
-setInterval(typing, 200)
-
 
  
